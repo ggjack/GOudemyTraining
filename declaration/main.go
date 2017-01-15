af3c226c9a837e0cbdd35b5c1bc402d622de729b
ggjack <gg.jack@hotmail.com>
2017-01-15 00:39:36 -0500

    Declaration and Assignments

---
+++ declaration/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// shorthand (declare, assign)
+	a := 10
+	b := "golang"
+	c := 4.16
+	d := true
+	fmt.Printf("%v \t %v \t %v \t %v \n", a, b, c, d)
+
+	// declaration (declaration)
+
+	var e int
+	var f string
+	var g float64
+	var h bool
+	fmt.Printf("%v \t %v \t %v \t %v \n", e, f, g, h)
+
+}
